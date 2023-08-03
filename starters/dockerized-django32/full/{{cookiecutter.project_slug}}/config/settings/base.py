0b0bc10ad74acb305f963dca2f37262611983f88
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-08-03 10:12:23 +0800

    ♻️refactor: remove repeated settings for internationalization
    fixed GRE-303
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- starters/dockerized-django32/full/{{cookiecutter.project_slug}}/config/settings/base.py
+++ starters/dockerized-django32/full/{{cookiecutter.project_slug}}/config/settings/base.py
@@ -275,20 +275,6 @@ ADMIN_URL = "admin/"
 # }
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
