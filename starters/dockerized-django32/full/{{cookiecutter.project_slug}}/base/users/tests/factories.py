0dcb8391a158bfd4a3a81d65dbab5a3d3de081e5
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-08-04 15:30:42 +0800

    ♻️refactor: remove tests using test_plus
    fixed GRE-307
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>
    
    ♻️refactor: remove factory
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- starters/dockerized-django32/full/{{cookiecutter.project_slug}}/base/users/tests/factories.py
+++ starters/dockerized-django32/full/{{cookiecutter.project_slug}}/base/users/tests/factories.py
@@ -1,4 +1,3 @@
-import django.contrib.auth.models as auth_models
 import factory
 
 TEST_USER_PASSWORD = "password"
@@ -21,41 +20,3 @@ class UserFactory(factory.django.DjangoModelFactory):
         if extracted:
             for group in extracted:
                 self.groups.add(group)
-
-
-class EnProfileFactory(factory.django.DjangoModelFactory):
-    user = factory.SubFactory(UserFactory)
-    signum = factory.Faker("word")
-    org_unit_short_name = factory.Faker("word")
-
-    class Meta:
-        model = "users.EnProfile"
-
-
-class VendorUserFactory(factory.django.DjangoModelFactory):
-    user = factory.SubFactory(UserFactory)
-    vendor = factory.SubFactory("vendors.tests.factories.VendorFactory")
-
-    class Meta:
-        model = "users.VendorUser"
-
-
-class GroupFactory(factory.django.DjangoModelFactory):
-    name = factory.Faker("name")
-
-    class Meta:
-        model = auth_models.Group
-
-
-class RoleFactory(GroupFactory):
-    class Meta:
-        model = "users.Role"
-
-
-class PeopleInSubProjectFactory(factory.django.DjangoModelFactory):
-    user = factory.SubFactory(UserFactory)
-    role = factory.SubFactory(RoleFactory)
-    sub_project = factory.SubFactory("service_skus.tests.factories.SubProjectFactory")
-
-    class Meta:
-        model = "users.PeopleInSubProject"
