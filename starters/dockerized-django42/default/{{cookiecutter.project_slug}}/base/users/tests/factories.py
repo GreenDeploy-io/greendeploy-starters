f5f5f75a1b44398f34a6e242f6da433b0f034c41
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-08-14 14:58:11 +0800

    🚧chore: fix the hardcoded password in django42/default
    fixed INF-21
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- starters/dockerized-django42/default/{{cookiecutter.project_slug}}/base/users/tests/factories.py
+++ starters/dockerized-django42/default/{{cookiecutter.project_slug}}/base/users/tests/factories.py
@@ -1,13 +1,12 @@
 import django.contrib.auth.models as auth_models
 import factory
-
-TEST_USER_PASSWORD = "password"
+from django.core.management.utils import get_random_secret_key
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: f"user-{n}")
     email = factory.Sequence(lambda n: f"user-{n}@example.com")
-    password = factory.PostGenerationMethodCall("set_password", TEST_USER_PASSWORD)
+    password = factory.PostGenerationMethodCall("set_password", get_random_secret_key())
     name = factory.Faker("name")
 
     class Meta:
