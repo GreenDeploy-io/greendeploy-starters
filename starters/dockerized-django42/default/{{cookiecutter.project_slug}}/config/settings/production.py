4766a9de595ec7986eb4fc1e37f70999ce50ce8d
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-09-06 18:19:42 +0800

    🐛fix:
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- starters/dockerized-django42/default/{{cookiecutter.project_slug}}/config/settings/production.py
+++
@@ -1,9 +0,0 @@
-from .base import *  # noqa
-from .base import env
-
-# BEGIN_REPLACE
-# {{ cookiecutter.domain_name }} => {{ {{ cookiecutter.domain_name }} }}
-# REPLACE_START
-# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["{{ cookiecutter.domain_name }}"])
-# REPLACE_END
