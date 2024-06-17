56c1a6a4e3b7b34bef47808a74cc3190b9725965
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2024-06-17 15:33:12 +0800

    feat: set default auto field explicitly
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- starters/micro-django42/default/{{cookiecutter.project_slug}}/manage.py
+++ starters/micro-django42/default/{{cookiecutter.project_slug}}/manage.py
@@ -40,6 +40,7 @@ settings.configure(
     DEBUG=DEBUG,
     SECRET_KEY=SECRET_KEY,
     ALLOWED_HOSTS=ALLOWED_HOSTS,
+    DEFAULT_AUTO_FIELD="django.db.models.BigAutoField",
     STATIC_URL="/static/",
     STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles"),
     STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")],
